b1920b2fcfbef379d4f5ef9c62ac75c8a3d2003d
unknown <smirnoff.yevhen@gmail.com>
2020-11-18 11:11:38 +0200

    added drawer closing/opeinng

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import classes from './Drawer.module.scss';
+
+const links = [
+    1,2,3
+];
+class Drawer extends Component {
+    renderLinks() {
+        return links.map((link, index) => {
+            return (
+                <li key={index}>
+                    <a>Link {link}</a>
+                </li>
+            )
+        })
+    }
+
+    render() {
+
+        const cls = [classes.Drawer]
+
+        if (!this.props.isOpen) {
+            cls.push(classes.close);
+        }
+        return (
+            <nav className={cls.join(' ')}>
+                {this.renderLinks()}
+            </nav>
+        )
+    }
+}
+
+export default Drawer;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,28 @@
 // import classes from './*.module.css';
 import React, { Component } from 'react';
 import classes from './Layout.module.css';
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
+import Drawer from '../../components/Navigation/Drawer/Drawer';
 
 export default class Layout extends Component {
+
+    state= {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
     render() {
         return (
             <div className={classes.Layout}>
+                <Drawer isOpen={this.state.menu}/>
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
                 <main>
                     { this.props.children}
                 </main>
