f92850f977d9ce85b19582a49a36bc2b7b811f70
unknown <smirnoff.yevhen@gmail.com>
2020-11-18 11:11:17 +0200

    added drawer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Quiz from './conatainers/Quiz/Quiz'
 function App() {
   return (
     <Layout>
-      <div style={{width: 400, border: '1px solid black'}}>
-        <h1>Layout worka</h1>
-      </div>
       <Quiz/>
-
     </Layout>
   );
 }
